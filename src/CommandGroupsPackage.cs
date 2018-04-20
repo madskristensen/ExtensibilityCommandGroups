0fa6fe4594d0cce0a6bb7a1dfd8ad719d349a2d0
Mads Kristensen <post@madskristensen.net>
2018-04-20 12:02:08 -0700

    Add project files.

---
+++ src/CommandGroupsPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace ExtensibilityCommandGroups
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [Guid("fb035c37-9d8e-4c86-856f-aada23222675")]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class CommandGroupsPackage : AsyncPackage
+    {
+    }
+}
